d9945c636bc0560ff903d8697ceecd3e786b9ab4
anas-raza~ <anusraza268@gmail.com>
2024-06-06 12:04:51 -0700

    modified

--- register.js
+++ register.js
@@ -6,16 +6,32 @@ const passward=document.querySelector('#passward')
 
 form.addEventListener('submit', (event) =>{
     event.preventDefault()
-    console.log(fullname.value);
+     console.log(fullname.value);
     console.log(email.value);
-    console.log(passward.value);
-})
+     console.log(passward.value);
 
-const arr=[]
-// let arr
-    let form=JSON.parse(localStorage.get('cartItems'))
-    if (items === null) {
-        
-    }else{
-        arr=items
+
+
+// const arr=[]
+// // let arr
+//   const formData= localStorage.setItem('formData',JSON.stringify('formData'))
+//  'StorageData',JSON.parse(localStorage.getItem('formData'))
+// if (formData === null) {
+//     console.log('ok');
+// }else{
+//     console.log('no');
+// }
+
+const formData={
+       fullname: fullname.value,
+       email: email.value,
+       passward: passward.value
     }
+        
+        
+    localStorage.setItem('formData',JSON.stringify(formData))
+    console.log('Stored Data:', JSON.parse(localStorage.getItem('formData')))
+})
+const arr=[]
+    
+    
